f87e6b06a20fe08b0dee72c97de9993929b1a241
Justwouter <wouter2312@gmail.com>
2023-04-27 21:24:45 +0200

    Moved file management to FileLib

--- Sentiment.py
+++ Sentiment.py
@@ -11,6 +11,7 @@ import FileLib
 inputpath = os.path.dirname(__file__)+"/input/data.txt"
 outputpath = os.path.dirname(__file__)+'/output/Sentiment/'
 dbpath = outputpath+"sentiment.sqlite"
+
     
 def PreformAnalysis(text):
     blob = TextBlob(text, pos_tagger=PatternTagger(), analyzer=PatternAnalyzer())
@@ -26,42 +27,8 @@ def PreformAnalysis(text):
     return [score_label,  str(text).encode("charmap", errors="replace").decode("charmap")]
 
 
-def saveDictToSQLITE(database, table, data):
-    # connect to the database
-    conn = sqlite3.connect(database)
-    c = conn.cursor()
-
-    # create the table if it doesn't exist
-    column_names = ", ".join(data.keys())
-    c.execute(f"CREATE TABLE IF NOT EXISTS {table} (id INTEGER PRIMARY KEY AUTOINCREMENT, {column_names})")
-
-    # insert the data into the table
-    placeholders = ", ".join("?" * len(data))
-    values = tuple(data.values())
-    c.execute(f"INSERT INTO {table} ({column_names}) VALUES ({placeholders})", values)
-
-    # commit the changes and close the connection
-    conn.commit()
-    conn.close()
-
-def saveDictToJSON(output, data):
-    with open(output, 'w', encoding="utf8", newline="") as out:
-        json.dump(data, out, ensure_ascii=False)  
-    
-def saveDictToCSV(output, data):
-    with open(output, 'w', encoding="utf8", newline="") as out:
-        write = DictWriter(out, fieldnames=data[0].keys())
-        write.writeheader()
-        for entry in data:
-            write.writerow(entry)
-
-
-
-
-
 def start():
     data = pd.read_csv(inputpath, skiprows=1)
-
     results = []
 
     with open(inputpath, "r",encoding="utf-8") as file:
@@ -72,18 +39,14 @@ def start():
             analysis = PreformAnalysis(text.replace("\"",""))
             results.append(dict(sku = skunr, sentiment = analysis[0], text = analysis[1]))
     
-    saveDictToJSON(outputpath+"Sentiment.json", results)
-    saveDictToCSV(outputpath+"Sentiment.csv", results)
-
+    
+    FileLib.EnsureFileExists(outputpath)
+    FileLib.saveDictToJSON(outputpath+"Sentiment.json", results)
+    FileLib.saveDictToCSV(outputpath+"Sentiment.csv", results)
     for line in results:
-        saveDictToSQLITE(dbpath,"Sentiment",line)
+        FileLib.saveDictToSQLITE(dbpath,"Analysis_Sentiment",line)
 
 
-
-       
-       
-       
-       
         
 starttime = time.time()
 start()
